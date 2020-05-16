ea69e681d0871efaab8be8be1201e5045d89b208
dongmin <sayg1o1d@gmail.com>
2020-05-16 21:16:43 +0900

    section5_control flow and code structure

---
+++ section5/comment.py
@@ -0,0 +1,32 @@
+print('XXXXX')
+"""
+test
+test
+test
+test
+"""
+# test
+# test
+# test
+# test
+print('XXXXX')
+
+# Apple price
+some_value= 100
+
+s= 'aaaaaaaaaa' + 'bbbbbbbb'
+x= 'aaaaaaaaaa' \
+   + 'bbbbbbbb'
+print(s)
+print(x)
+
+g = 1+1+1+1+1+1+1+1 \
+    +1+1+1+1+1+1+1+1+1
+# 同じ
+q = (1+1+1+1+1+1+1+1
+    +1+1+1+1+1+1+1+1+1)
+print(g)
+print(q)
+
+# 123456789aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
