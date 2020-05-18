4aa0fac56873e448c5f6ba83a17319385c8e8a5d
dongmin <sayg1o1d@gmail.com>
2020-05-18 22:12:44 +0900

    section5_logical-operator

---
+++ section5/Technique.py
@@ -0,0 +1,24 @@
+# is_ok = True
+# is_ok = 1
+# is_ok = 0
+# 0 >> Flas
+# e
+# 1 >> True
+# is_ok = 10020
+# is_ok = ''
+# is_ok = 'asdfdsdf'
+# is_ok= []
+
+# Flase, 0, 0.0, '', [],(),{},set()
+# Flase, 0, 0.0, '', [],(),{},set()
+# Flase, 0, 0.0, '', [],(),{},set()
+# Flase, 0, 0.0, '', [],(),{},set()
+# Flase, 0, 0.0, '', [],(),{},set()
+is_ok = [1,2,3,4]
+
+# if len(is_ok) > 0:
+
+if is_ok:
+    print('OK!')
+else:
+    print('NO')
---
+++ section5/logical_operator.py
@@ -0,0 +1,36 @@
+# a = 1
+# b = 1
+a = 2
+b = 2
+
+# if a == b:
+#     print('equal')
+a == b
+
+a != b
+
+a < b
+
+a > b
+
+a <= b
+
+a >= b
+
+if a > 0:
+    if b > 0:
+        print('a and b are positive')
+
+if a > 0 and b > 0:
+    print('a and b are positive')
+
+# a = 1
+a = -1
+b = 1
+# b = -1
+if a > 0:
+    print('a or b is positive')
+elif b > 0:
+    print('a or b is positive')
+if a > 0 or b > 0:
+    print('a or b is positive')
