abf4ca1241f85f4af3b87efee9a512a18e2bfc3c
dongmin <sayg1o1d@gmail.com>
2020-05-19 22:11:18 +0900

    section5_logical-operator

---
+++ section5_controlflow/input.py
@@ -0,0 +1,24 @@
+# while True:
+#     word = input('Enter:')
+#     if word == 'ok':
+#         break
+#     print('next')
+    # --------------
+    # Enter: e
+    # next
+    # Enter: ok
+    # --------------
+    #
+
+while True:
+    word = input('Enter:')
+    num = int(word)
+    if num == 100:
+        break
+    print('next')
+# --------------------------
+# Enter:1
+# next
+# Enter:100
+# --------------------------
+
