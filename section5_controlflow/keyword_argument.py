362369f89b89a40aeff164ffd9b309a601b23371
dongmin <sayg1o1d@gmail.com>
2020-05-31 13:53:44 +0900

    section5_logical-operator

---
+++ section5_controlflow/keyword_argument.py
@@ -0,0 +1,12 @@
+# def menu(entree, drink, dessert):
+def menu(entree='beef', drink='wine', dessert='ice'):
+    print('entree=',entree)
+    print('drink = ',drink)
+    print('dessert = ',dessert)
+
+menu(entree='chicken', drink='beer')
+
+# menu('beef', 'beer', 'ice')
+# menu('beef', 'ice', 'beer')
+# menu(entree='beef', dessert='ice',drink= 'beer')
+
